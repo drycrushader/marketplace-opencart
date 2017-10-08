c61940834e91385914658cc5f2181399f096db76
drycrushader <drycrushader@gmail.com>
2017-10-08 18:16:44 +0700

    Google+ Login Fix

--- www/scripts/plugins.js
+++ www/scripts/plugins.js
@@ -346,3 +346,224 @@ var openFB = (function () {
     }
 
 }());
+
+/**
+ * OpenGP is a micro-library that lets you integrate your JavaScript application with Facebook.
+ * OpenGP works for both BROWSER-BASED apps and CORDOVA/PHONEGAP apps.
+ * This library has no dependency: You don't need (and shouldn't use) the Facebook SDK with this library. Whe running in
+ * Cordova, you also don't need the Facebook Cordova plugin. There is also no dependency on jQuery.
+ * OpenFB allows you to login to Facebook and execute any Facebook Graph API request.
+ * @author Christophe Coenraets @ccoenraets
+ * @version 0.5
+ */
+var openGP = (function () {
+
+    var loginURL = 'https://accounts.google.com/o/oauth2/auth',
+        logoutURL = 'https://accounts.google.com/o/oauth2/revoke',
+        tokenStore = window.sessionStorage,
+        gpAppId,
+        context = window.location.pathname.substring(0, window.location.pathname.lastIndexOf("/")),
+        baseURL = location.protocol + '//' + location.hostname + (location.port ? ':' + location.port : '') + context,
+        oauthRedirectURL = baseURL + '/oauthcallback_gp.html',
+        cordovaOAuthRedirectURL = host + "index.php?route=extension/module/mobi_sociallogin/GoogleLogins",
+        logoutRedirectURL = baseURL + '/logoutcallback.html',
+        loginCallback,
+        runningInCordova,
+        loginProcessed;
+		
+    document.addEventListener("deviceready", function () {
+        runningInCordova = true;
+    }, false);
+
+    function init(params) {
+
+        if (params.appId) {
+            gpAppId = params.appId;
+        } else {
+            throw 'secretId parameter not set in init()';
+        }
+
+        if (params.tokenStore) {
+            tokenStore = params.tokenStore;
+        }
+
+        if (params.accessToken) {
+            tokenStore.gpAccessToken = params.accessToken;
+        }
+
+        loginURL = params.loginURL || loginURL;
+        logoutURL = params.logoutURL || logoutURL;
+        oauthRedirectURL = params.oauthRedirectURL || oauthRedirectURL;
+        cordovaOAuthRedirectURL = params.cordovaOAuthRedirectURL || cordovaOAuthRedirectURL;
+        logoutRedirectURL = params.logoutRedirectURL || logoutRedirectURL;
+
+    }
+	
+    function getLoginStatus(callback) {
+        var token = tokenStore.gpAccessToken,
+            loginStatus = {};
+        if (token) {
+            loginStatus.status = 'connected';
+            loginStatus.authResponse = {accessToken: token};
+        } else {
+            loginStatus.status = 'unknown';
+        }
+        if (callback) callback(loginStatus);
+    }
+	
+    function login(callback, options) {
+
+        var loginWindow,
+            startTime,
+            scope = '',
+            redirectURL = runningInCordova ? cordovaOAuthRedirectURL : oauthRedirectURL;
+
+        if (!gpAppId) {
+            return callback({status: 'unknown', error: 'Google Secret Id not set.'});
+        }
+
+        function loginWindow_loadStartHandler(event) {
+            var url = event.url;
+            if (url.indexOf("access_token=") > 0 || url.indexOf("error=") > 0) {
+                var timeout = 600 - (new Date().getTime() - startTime);
+                setTimeout(function () {
+                    loginWindow.close();
+                }, timeout > 0 ? timeout : 0);
+                oauthCallback(url);
+            }
+        }
+
+        function loginWindow_exitHandler() {
+            console.log('exit and remove listeners');
+            if (loginCallback && !loginProcessed) loginCallback({status: 'user_cancelled'});
+            loginWindow.removeEventListener('loadstop', loginWindow_loadStopHandler);
+            loginWindow.removeEventListener('exit', loginWindow_exitHandler);
+            loginWindow = null;
+            console.log('done removing listeners');
+        }
+
+        if (options && options.scope) {
+            scope = options.scope;
+        }
+
+        loginCallback = callback;
+        loginProcessed = false;
+
+        startTime = new Date().getTime();
+        loginWindow = window.open(loginURL + '?client_id=' + gpAppId + '&redirect_uri=' + redirectURL +
+            '&response_type=token&scope=' + scope, '_blank', 'location=no,clearcache=yes');
+
+        if (runningInCordova) {
+            loginWindow.addEventListener('loadstart', loginWindow_loadStartHandler);
+            loginWindow.addEventListener('exit', loginWindow_exitHandler);
+        }
+    }
+
+    function oauthCallback(url) {
+        var queryString,
+            obj;
+
+        loginProcessed = true;
+        if (url.indexOf("access_token=") > 0) {
+            queryString = url.substr(url.indexOf('#') + 1);
+            obj = parseQueryString(queryString);
+            tokenStore.gpAccessToken = obj['access_token'];
+            if (loginCallback) loginCallback({status: 'connected', authResponse: {accessToken: obj['access_token']}});
+        } else if (url.indexOf("error=") > 0) {
+            queryString = url.substring(url.indexOf('?') + 1, url.indexOf('#'));
+            obj = parseQueryString(queryString);
+            if (loginCallback) loginCallback({status: 'not_authorized', error: obj.error});
+        } else {
+            if (loginCallback) loginCallback({status: 'not_authorized'});
+        }
+    }
+
+    function logout(callback) {
+        var logoutWindow,
+            token = tokenStore.gpAccessToken;
+
+        tokenStore.removeItem('gptoken');
+
+        if (token) {
+            logoutWindow = window.open(logoutURL + '?access_token=' + token + '&next=' + logoutRedirectURL, '_blank', 'location=no,clearcache=yes');
+            if (runningInCordova) {
+                setTimeout(function() {
+                    logoutWindow.close();
+                }, 700);
+            }
+        }
+
+        if (callback) {
+            callback();
+        }
+
+    }
+
+    function api(obj) {
+
+        var method = obj.method || 'POST',
+            params = obj.params || {},
+            xhr = new XMLHttpRequest(),
+			token = tokenStore.gpAccessToken,
+            url;
+
+        params['access_token'] = tokenStore.gpAccessToken;
+
+        url = 'https://www.googleapis.com/oauth2/v3/userinfo?access_token=' + token;
+
+        xhr.onreadystatechange = function () {
+            if (xhr.readyState === 4) {
+                if (xhr.status === 200) {
+                    if (obj.success) obj.success(JSON.parse(xhr.responseText));
+                } else {
+                    var error = xhr.responseText ? JSON.parse(xhr.responseText).error : {message: 'An error has occurred'};
+                    if (obj.error) obj.error(error);
+                }
+            }
+        };
+
+        xhr.open(method, url, true);
+        xhr.send();
+    }
+	
+    function revokePermissions(success, error) {
+        return api({method: 'DELETE',
+            path: '/me/permissions',
+            success: function () {
+                success();
+            },
+            error: error});
+    }
+
+    function parseQueryString(queryString) {
+        var qs = decodeURIComponent(queryString),
+            obj = {},
+            params = qs.split('&');
+        params.forEach(function (param) {
+            var splitter = param.split('=');
+            obj[splitter[0]] = splitter[1];
+        });
+        return obj;
+    }
+
+    function toQueryString(obj) {
+        var parts = [];
+        for (var i in obj) {
+            if (obj.hasOwnProperty(i)) {
+                parts.push(encodeURIComponent(i) + "=" + encodeURIComponent(obj[i]));
+            }
+        }
+        return parts.join("&");
+    }
+
+    return {
+        init: init,
+        login: login,
+        logout: logout,
+        revokePermissions: revokePermissions,
+        api: api,
+        oauthCallback: oauthCallback,
+        getLoginStatus: getLoginStatus
+    }
+
+}());
